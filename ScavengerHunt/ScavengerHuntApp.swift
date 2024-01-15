e5f7657beb196d821e1ce3055dd9a5c1f1eac383
Juman <juman.developer@gmail.com>
2024-01-15 11:20:34 +0300

    Initial Commit

---
+++ ScavengerHunt/ScavengerHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScavengerHuntApp.swift
+//  ScavengerHunt
+//
+//  Created by Juman Dhaher on 03/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ScavengerHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
